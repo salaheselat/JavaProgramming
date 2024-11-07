0c6dca8300350e2a65019a6567dffd1627751cce
salaheselat <86536817+salaheselat@users.noreply.github.com>
2024-11-07 08:40:46 +0000

    Update exercise10.java
    need to update file with functions

--- src/Functions/exercise10.java
+++ src/Functions/exercise10.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class exercise10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Update this to latest version functions
+        // Update this to latest version functions !
         displayMenu();
     }
 
